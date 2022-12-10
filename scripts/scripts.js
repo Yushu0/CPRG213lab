af58b80fe3cd8f7d9069769730b2fbed4f6f4584
Yushu0 <119829083+Yushu0@users.noreply.github.com>
2022-12-09 19:10:12 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,8 +10,8 @@ You are encouraged to use the provided naming convention for ease of review.
 /* create variables to hold the values for modelName and duration */
 
 // INSERT YOUR CODE HERE
-let modelName;
-let duration;
+let modelName = 'XYZ';
+let duration = 0;
 
 
 
